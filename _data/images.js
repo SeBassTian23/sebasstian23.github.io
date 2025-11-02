888718da377dc87362503506637d5603fd2561a3
Sebastian <sebastian.kuhlgert@gmail.com>
2025-11-02 11:07:10 +0100

    update image function

--- _data/images.js
+++ _data/images.js
@@ -1,11 +1,10 @@
 import fs from "fs";
 import path from "path";
 
-import pluginImage from "@11ty/eleventy-img";
-import shorthash from "short-hash";
+import Image from "@11ty/eleventy-img";
 
-export const imageSizes = "(min-width: 1024px) 100vw, 50vw"; //'(max-width: 768px) 100vw, 768px'
-export const imageWidths = [600, 900, 1500];
+export const imageSizes = "(min-width: 1200px) 1320px, (min-width: 768px) 100vw, 100vw"; //"(min-width: 1024px) 100vw, 50vw"; //'(max-width: 768px) 100vw, 768px'
+export const imageWidths = [320, 640, 1024, 1320];
 export const imageFormats = ['webp', 'jpeg'];
 
 /**
@@ -16,103 +15,119 @@ export const imageFormats = ['webp', 'jpeg'];
  * @param {*} addClass 
  * @returns 
  */
-export const imageHTML = (src, alt, sizes = "100%", addClass = "img-fluid") => {
-  if (alt === undefined) {
-    alt = "";
-    // You bet we throw an error on missing alt (alt="" works okay)
-    // throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-  }
-  let options = {
-    widths: imageWidths,
-    // .concat(imageWidths.map((w) => w * 2))         // generate 2x sizes for retina displays
-    // .filter((v, i, s) => s.indexOf(v) === i),
-    formats: imageFormats,
-    sizes: imageSizes,                             // choose your own formats (see docs)
-    urlPath: '/images',                            // src path in HTML output
-    outputDir: './_site/images/',                   // where the generated images will go
-    svgShortCircuit: true
-  };
+export const imageHTML = (src, alt = "", sizes = "100%", addClass = "img-fluid") => {
 
-  let urlParameters = {
-    width: null,
-    float: null,
-    class: null
-  };
+  let params = {}
+  let tag;
 
-  // Split to separate link and parameters/anchors
-  let srcOriginal = src;
-  let im = src.split(/#|\?/);
-  src = path.join('./_assets', im[0]);
+  // Check if the source is local or remote
+  if (!src.match(/^https?:\/\//)) {
 
-  if (!fs.existsSync(src)) {
-    // console.log(src)
-    return "";
-  }
+    // Add assets folder to path
+    src = path.join('./_assets', src);
 
-  if (im[1]) {
-    let params = [...srcOriginal.matchAll(/(?:\?|\&)(?<key>[\w]+)(?:\=|\&?)(?<value>[\w+,.-]*)/g)];
-    params.forEach((x) => {
-      if (x.groups.key == "width" || x.groups.key == "w")
-        urlParameters.width = x.groups.value;
-      if (x.groups.key == "float" || x.groups.key == "f")
-        urlParameters.float = x.groups.value;
-      if (x.groups.key == "class" || x.groups.key == "c")
-        urlParameters.class = x.groups.value;
-    });
-  }
+    // Check if the path has a url tag
+    tag = src.split('#');
+    if (tag.length > 1) {
+      tag = tag[1]
+      src = tag[0]
+    }
+    else
+      tag = null;
 
-  if (im[1] && im[1].match(/^s=/))
-    sizes = im[1].split("=")[1] || sizes;
+    // Check if the local file path has url parameters
+    if (src.split('?').length > 1) {
+      params = Object.fromEntries([...src.matchAll(/(?:\?|\&)(?<key>[\w]+)(?:\=|\&?)(?<value>[\w+,.-]*)/g)].map(e => {
 
-  // Passthrough SVGs since it is not done properly 
-  // using the standard function
-  if (src.match(/\.svg$/)) {
-    var basename = path.basename(src);
-    var hashname = `${shorthash(basename)}.svg`;
-    var outputPath = path.join(options.outputDir, hashname);
-    if (!fs.existsSync(options.outputDir))
-      fs.mkdirSync(options.outputDir);
-    fs.copyFileSync(src, outputPath);
-    var svg = {
-      svg: [
-        {
-          format: 'svg',
-          width: sizes,
-          height: 'auto',
-          url: path.join(options.urlPath, hashname),
-          sourceType: 'image/svg',
-          srcset: `${path.join(options.urlPath, hashname)} ${sizes}`,
-          filename: hashname,
-          outputPath: outputPath
+        let key = e[1];
+        switch (e[1][0]) {
+          case 'f':
+            key = "float"
+            break;
+          case 'w':
+            key = "width"
+            break;
+          case 'c':
+            key = "class"
+            break;
+          case 's':
+            key = "size"
+            break;
         }
-      ]
-    };
-    return pluginImage.generateHTML(svg, {
-      alt: alt,
-      class: addClass,
-      sizes: imageSizes,
-      loading: 'lazy',
-      decoding: 'async'
-    });
+
+        return [key, decodeURIComponent(e[2])]
+      }))
+      src = src.split('?')[0]
+    }
+
+    // Check if the local file exists
+    if (!fs.existsSync(src))
+      return ""
   }
+
+  // Change settings if SVG
+  if(src.endsWith('.svg')){
+    params.widths = ["auto"]
+    params.formats = ["svg"]
+  }
+
+  let options = {
+    widths: params?.widths || imageWidths,
+    formats: params?.formats || imageFormats,
+    sizes: imageSizes,
+    urlPath: '/images',
+    outputDir: './_site/images/',
+    svgShortCircuit: true,
+    svgAllowUpscale: true,
+    fixOrientation: true,
+    sharpOptions: {
+      animated: true,
+    },
+    cache: true,
+    htmlOptions: {
+			imgAttributes: {
+				loading: "lazy",
+				decoding: "async",
+			},
+			pictureAttributes: {}
+		}
+  };
 
   // Generate Image
-  pluginImage(src, options);
+  Image(src, options);
+
+  // Add Params
+  if (params?.float)
+    addClass += ` post-inline-img col-md-6 float-md-${params.float} mb-3 m${params.float[0] == 'e' ? 's' : 'e'}-md-3`
 
   // Generate HTML container for image
-  let metadata = pluginImage.statsSync(src, options);
-  let html = pluginImage.generateHTML(metadata, {
+  let metadata;
+  if (src.match(/^https?:\/\//)) {
+    metadata = Image.statsByDimensionsSync(src, Math.max(...imageWidths), Math.max(...imageWidths), options);
+  }
+  else
+    metadata = Image.statsSync(src, options);
+
+  if(metadata.svg){
+    metadata.svg = metadata.svg.map( el => {
+      return {
+        ...el,
+        ...{
+          height: 'auto', 
+          width: Number(params.size) || el.width
+        }
+      }
+    });
+  }
+
+  let html = Image.generateHTML(metadata, {
     alt: alt,
-    class: urlParameters.class ? `${urlParameters.class} ${addClass}` : addClass,
-    sizes: imageSizes,
+    class: params.class ? `${params.class} ${addClass}` : addClass,
+    sizes: params?.sizes || imageSizes,
     loading: 'lazy',
     decoding: 'async'
   });
 
-  if (urlParameters.float == 'start')
-    return `<div class="post-inline-img col-md-6 float-md-start mb-3 me-md-3">${html}</div>`;
-  if (urlParameters.float == 'end')
-    return `<div class="post-inline-img col-md-6 float-md-end mb-3 ms-md-3">${html}</div>`;
   return html;
 }
 
@@ -140,19 +155,20 @@ export const imageResizedURL = (src, format = "jpeg", size = 1200) => {
     // sharpAvifOptions: {}
   };
 
-  src = path.join('./_assets', src);
+  if (!src.match(/^https?:\/\//))
+    src = path.join('./_assets', src);
 
   // Generate Image
-  pluginImage(src, options);
+  Image(src, options);
 
   // Generate HTML container for image
-  let metadata = pluginImage.statsSync(src, options);
+  let metadata = Image.statsByDimensionsSync(src, size, size, options);
   return metadata[format][0];
 }
 
 
 /**
- * 
+ * Get all file paths from an album folder or album array
  * @param {*} src 
  * @returns
  */
@@ -173,7 +189,7 @@ export const album = (src) => {
 
     if (fs.lstatSync(content).isDirectory()) {
       let files = fs.readdirSync(content)
-        .filter((e) => ['.jpg', '.jpeg', '.png', '.gif', '.webp'].includes(path.extname(e).toLowerCase()))
+        .filter((e) => ['.jpg', '.jpeg', '.png', '.gif', '.webp', 'tiff', 'avif', 'svg'].includes(path.extname(e).toLowerCase()))
         .map((e) => path.join(src[i], e));
       imagefiles = [...imagefiles, ...files]
     }
@@ -183,4 +199,40 @@ export const album = (src) => {
     }
   }
   return imagefiles;
+}
+
+
+/**
+ * Find title image in album by aspect ratio
+ */
+export const titleImgFromAlbum = (src) => {
+
+  // Get all album images
+  src = album(src)
+
+  let aspectRatio = 1;
+  let selected = null;
+
+  for(let i in src){
+    let imgPath = path.join('_assets', src[i] );
+    if( fs.existsSync( imgPath ) ){
+      let img = Image.statsSync(imgPath, {
+        fixOrientation: true
+      });
+      let keys = Object.keys(img)
+      if(keys.length > 0){
+        if(img[keys[0]].length > 0){
+          let w = Number(img[keys[0]][0]?.width)
+          let h = Number(img[keys[0]][0]?.height)
+          let r = w / h;
+          if(r > aspectRatio){
+            aspectRatio = r
+            selected = src[i]
+          }
+        }
+      }
+    }
+  }
+
+  return selected;
 }
